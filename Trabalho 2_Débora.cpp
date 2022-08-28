e5abba2e05d4fb9ddaadc1c35541eaf7fbfd5ae3
Eurosphyr <111361724+Eurosphyr@users.noreply.github.com>
2022-08-28 20:14:23 -0300

    Add files via upload

--- Trabalho 2_Débora.cpp
+++ Trabalho 2_Débora.cpp
@@ -7,40 +7,34 @@ float vf;
 
 usuario(int user)
 {
-    
-    do
+
+    switch (user)
     {
-    	if(user >4 or user <1)
-    	continue;
-        switch (user)
-        {
 
-        case 1:
-            printf("Usuario tipo 1 ");
-            printf("\nAumento de 10%% na mensalidade");
-            user = 1;
-            return user;
-            break;
+    case 1:
+        printf("Usuario tipo 1 ");
+        printf("\nAumento de 10%% na mensalidade");
+        user = 1;
+        return user;
+        break;
 
-        case 2:
-            printf("Usuario tipo 2 ");
-            printf("\nAumento de 15%% na mensalidade");
-            user = 2;
-            return user;
-            break;
+    case 2:
+        printf("Usuario tipo 2 ");
+        printf("\nAumento de 15%% na mensalidade");
+        user = 2;
+        return user;
+        break;
 
-        case 3:
-            printf("Usuario tipo 3 ");
-            printf("\nAumento de 20%% na mensalidade");
-            user = 3;
-            return user;
-            break;
+    case 3:
+        printf("Usuario tipo 3 ");
+        printf("\nAumento de 20%% na mensalidade");
+        user = 3;
+        return user;
+        break;
 
-        default:
-            printf("Tipo de usuario inexistente");
-            printf("\n Digite novamente");
-        }
-    }while(user <=0 or user >=4);
+    default:
+        printf("\nTipo de usuario inexistente, digite novamente");
+    }
 }
 
 calculo(float vi, int user)
@@ -68,18 +62,21 @@ calculo(float vi, int user)
         printf("Valor Invalido");
     }
     vf = vi + result;
-};
+}
 
 int main(void)
 {
-
     float vi;
     int user;
     printf("Digite o valor da mensalidade: ");
     scanf("%f", &vi);
-    printf("Digite o tipo de usuario: ");
-    scanf("%d", &user);
-    usuario(user);
+    do
+    {
+        printf("\nDigite o tipo de usuario: ");
+        scanf("%d", &user);
+        usuario(user);
+    } while (user <= 0 or user >= 4);
+
     calculo(vi, user);
     printf("\nValor final da mensalidade eh: R$%.2f", vf);
 }
