2e95679cbb9b8f2046b9edcbbe6b32bdfabd7065
Eurosphyr <111361724+Eurosphyr@users.noreply.github.com>
2022-08-29 19:20:34 -0300

    Add files via upload

--- Trabalho 2_Débora.cpp
+++ Trabalho 2_Débora.cpp
@@ -5,63 +5,37 @@
 */
 float vf;
 
-usuario(int user)
+float usuario(int user)
 {
-
-    switch (user)
+    do
     {
 
-    case 1:
-        printf("Usuario tipo 1 ");
-        printf("\nAumento de 10%% na mensalidade");
-        user = 1;
-        return user;
-        break;
-
-    case 2:
-        printf("Usuario tipo 2 ");
-        printf("\nAumento de 15%% na mensalidade");
-        user = 2;
-        return user;
-        break;
-
-    case 3:
-        printf("Usuario tipo 3 ");
-        printf("\nAumento de 20%% na mensalidade");
-        user = 3;
-        return user;
-        break;
-
-    default:
-        printf("\nTipo de usuario inexistente, digite novamente");
-    }
+        if (user <= 0 or user >= 4)
+            printf("Usuario Invalido, redigite. ");
+        	scanf("%d", &user);
+    } while (user <= 0 or user >= 4);
+    return user;
 }
 
-calculo(float vi, int user)
+float calculo(float vi, int user)
 {
-
-    float result = vi * 100;
     switch (user)
     {
     case 1:
-        result = (result / 100) * 10;
-        result /= 100;
+        vf = vi * 1.1;
         break;
 
     case 2:
-        result = (result / 100) * 15;
-        result /= 100;
+        vf = vi * 1.15;
         break;
 
     case 3:
-        result = (result / 100) * 20;
-        result /= 100;
+        vf = vi * 1.2;
         break;
 
     default:
         printf("Valor Invalido");
     }
-    vf = vi + result;
 }
 
 int main(void)
@@ -70,13 +44,9 @@ int main(void)
     int user;
     printf("Digite o valor da mensalidade: ");
     scanf("%f", &vi);
-    do
-    {
-        printf("\nDigite o tipo de usuario: ");
-        scanf("%d", &user);
-        usuario(user);
-    } while (user <= 0 or user >= 4);
-
+    printf("\nDigite o tipo de usuario: ");
+    scanf("%d", &user);
+    user = usuario(user);
     calculo(vi, user);
     printf("\nValor final da mensalidade eh: R$%.2f", vf);
 }
